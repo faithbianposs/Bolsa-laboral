2759a2ae40344223258cc614f54c1c60794a0263
Frank <119316605+faithbianposs@users.noreply.github.com>
2024-11-28 08:50:26 -0500

    Delete lb_app directory

--- lb_app/lib/util/TokenUtil.dart
+++
@@ -1,6 +0,0 @@
-class TokenUtil{
-  TokenUtil._();
-  static bool localx=false;
-  static String TOKEN="";
-  static String authUserId = "";
-}
--- lb_app/lib/util/UrlApi.dart
+++
@@ -1,5 +0,0 @@
-
-class UrlApi{
-  UrlApi._();
-  static const String urlApix="http://192.168.0.87:8080";
-}
